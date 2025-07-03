464e99571aac053a95ba709cc9e711c5288375ec
Júlia Pavlick <juliapavlickk@gmail.com>
2025-07-03 14:53:14 -0300

    .

---
+++ js/event.js
@@ -0,0 +1,11 @@
+function mouseOver()
+{
+    document.image.src="img/cat02";
+}
+function mouseOut()
+{
+    document.image.src="img/cat01";
+}
+<andress>
+Júlia Caroline Borges pavlick
+</andress>
